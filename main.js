66c2f7daf247c0bd4e48cede4b36294aee1d6861
Khabibullokh <86731331+Habibulloh2003@users.noreply.github.com>
2021-08-22 15:01:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+
+
+// console.log("[+][+][+][+][+][+][+][+][+][+]");
+// console.log("[+][+]                     [+]");
+// console.log("[+]   [+]                  [+]");
+// console.log("[+]      [+]               [+]");
+// console.log("[+]         [+]            [+]");
+// console.log("[+]            [+]         [+]");
+// console.log("[+]               [+]      [+]");
+// console.log("[+]                  [+]   [+]");
+// console.log("[+]                     [+][+]");
+// console.log("[+][+][+][+][+][+][+][+][+][+]");
+
+
+
+
+
+ for (let i = 0; i < 10; i++) {
+    if (i < 1) {
+        console.log("[+][+][+][+][+][+][+][+][+][+]");
+    } 
+    else if (i < 2) {
+        console.log("[+]" + "[+]" + "                     " + "[+]")
+    }
+    else if (i < 3) {
+        console.log("[+]" + "   " + "[+]" + "                  " + "[+]")
+    }
+    else if (i < 4) {
+        console.log("[+]" + "      " + "[+]" + "               " + "[+]")
+    }
+    else if (i < 5) {
+        console.log("[+]" + "         " + "[+]" + "            " + "[+]")
+    }
+    else if (i < 6) {
+        console.log("[+]" + "            " + "[+]" + "         " + "[+]")
+    }
+    else if (i < 7) {
+        console.log("[+]" + "               " + "[+]" + "      " + "[+]")
+    }
+    else if (i < 8) {
+        console.log("[+]" + "                  " + "[+]" + "   " + "[+]")
+    }
+    else if (i < 9) {
+        console.log("[+]" + "                     " + "[+]" + "[+]")
+    }
+    else if (i < 10) {
+        console.log("[+][+][+][+][+][+][+][+][+][+]")
+    }
+}
+
+
